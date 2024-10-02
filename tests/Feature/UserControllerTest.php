20e84b5611968bdbf1643f429a7a9e3db7011bb7
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-10-02 16:45:10 +0700

    feat: Only Quest Middleware

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -8,6 +8,14 @@ use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
+    public function testLoginPageForMember()
+    {
+        $this->withSession([
+            'user' => 'farhan'
+        ])->get('/login')
+            ->assertRedirect('/');
+    }
+    
     public function testLoginSuccess()
     {
         $this->post('/login', [
@@ -15,6 +23,16 @@ class UserControllerTest extends TestCase
             'password' => '12345'
         ])->assertRedirect('/')
             ->assertSessionHas('user', 'farhan');
+    }
+    
+    public function testLoginForUserAlreadyLogin()
+    {
+        $this->withSession([
+            'user' => 'farhan'
+        ])->post('/login', [
+            'user' => 'farhan',
+            'password' => '12345'
+        ])->assertRedirect('/');
     }
 
     public function testLoginValidationError()
@@ -30,5 +48,14 @@ class UserControllerTest extends TestCase
             'password' => 'wrong'
         ])
             ->assertSeeText('User or password is wrong');
+    }
+
+    public function testLogOut()
+    {
+        $this->withSession([
+            'user' => 'farhan'
+        ])->post('/logout')
+            ->assertSessionMissing('user')
+            ->assertRedirect('/');
     }
 }
