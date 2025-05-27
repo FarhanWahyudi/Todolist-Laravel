f430c725914cedff72de0e74ebd5889a2dd724c5
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-05-28 06:46:10 +0700

    membuat middleware member dan guest

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\OnlyGuestMiddleware;
+use App\Http\Middleware\OnlyMemberMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +25,7 @@ Route::get('/template', function () {
 });
 
 Route::controller(UserController::class)->group(function () {
-    Route::get('/login', 'login');
-    Route::post('/login', 'doLogin');
-    Route::get('/logout', 'doLogout');
+    Route::get('/login', 'login')->middleware(OnlyGuestMiddleware::class);
+    Route::post('/login', 'doLogin')->middleware(OnlyGuestMiddleware::class);
+    Route::get('/logout', 'doLogout')->middleware(OnlyMemberMiddleware::class);
 });
