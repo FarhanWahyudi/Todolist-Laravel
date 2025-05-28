0eccbd89aa6fa4956e67f0bd6f01c8506dd9904f
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-05-28 07:35:45 +0700

    membuat home controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\OnlyGuestMiddleware;
 use App\Http\Middleware\OnlyMemberMiddleware;
@@ -16,13 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/template', function () {
     return view('template');
 });
+
+Route::get('/', [HomeController::class, 'home']);
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/login', 'login')->middleware(OnlyGuestMiddleware::class);
