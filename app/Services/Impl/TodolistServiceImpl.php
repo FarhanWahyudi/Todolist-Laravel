577917cf5617584d76d97e1343eef9de5c2327a3
farhan wahyudi <farhanwahyudi875@gmail.com>
2025-06-01 14:02:13 +0700

    Update Todo Serviceto Connect Database

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Impl;
 
+use App\Models\Todo;
 use App\Services\TodolistService;
 use Illuminate\Support\Facades\Session;
 
@@ -9,32 +10,20 @@ class TodolistServiceImpl implements TodolistService
 {
     public function saveTodo(string $id, string $todo): void
     {
-         if (!Session::exists('todolist')) {
-            Session::put('todolist', []);
-         }
-
-         Session::push('todolist', [
+        Todo::create([
             'id' => $id,
             'todo' => $todo
-         ]);
+        ]);
     }
 
     public function getTodo(): array
     {
-        return Session::get('todolist', []);
+        return Todo::get()->toArray();
     }
 
     public function removeTodo(string $todoId)
     {
-        $todolist = Session::get('todolist');
-
-        foreach ($todolist as $index => $value) {
-            if ($value['id'] == $todoId) {
-                unset($todolist[$index]);
-                break;
-            }
-        }
-
-        Session::put('todolist', $todolist);
+        $todo = Todo::find($todoId);
+        $todo->delete();
     }
 }
